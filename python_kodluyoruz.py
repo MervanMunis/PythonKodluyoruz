01330284a47b6ba14716a05953a445e834c98f1c
MervanMunis <mervan.munis@gmail.com>
2024-04-26 09:55:45 +0300

    made some arrangements to make the code prettier - usage of round and function

--- python_kodluyoruz.py
+++ python_kodluyoruz.py
@@ -1,22 +1,22 @@
 import math
 
-distances = []
 points = [(3,4), (6,8), (5,12), (8,10), (-5, 10)]
 
+distances = []
 # To find the distances by using the formula
-def euclideanDistance(a, b):
-    x1 = a[0]
-    y1 = a[1]
-    x2 = b[0]
-    y2 = b[1]
+def euclideanDistance(point1, point2):
+    """İki nokta arasındaki Öklid mesafesini hesaplar."""
+    x1, y1 = point1
+    x2, y2 = point2
     return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
 
 # Looping through the points and getting all combinations of the distances
-for a in range(len(points) -1):
-    for b in range(a+1,len(points)):
-        distance = round(euclideanDistance(points[a], points[b]),2)
+for i in range(len(points) -1):
+    for j in range(i+1,len(points)):
+        distance = round(euclideanDistance(points[i], points[j]),2)
         distances.append(distance)
 
 # printing the minimum distance
-print(min(distances))
+min_distance = min(distances)
+print("Minimum Öklid mesafesi:", min_distance)
 
